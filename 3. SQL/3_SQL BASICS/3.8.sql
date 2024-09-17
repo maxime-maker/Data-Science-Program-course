0826d4808dfa4c95f528dd0752d04a7a67cfc90a
Maxime Ruzindana <162898021+maxime-maker@users.noreply.github.com>
2024-09-17 09:33:33 -0700

    Add files via upload

---
+++ 3. SQL/3_SQL BASICS/3.8.sql
@@ -0,0 +1,17 @@
+INSERT INTO united_nationns.access_to_basic_services(
+	Region, 
+	Sub_region,
+	country_name, 
+	Time_period,
+	Pct_managed_drinking_water_service, 
+	Pct_managed_sanitation_services,
+	Est_population_in_millions,
+	Est_gdp_in_billions,
+	Land_are,
+	Pct_unemployment
+)
+VALUES
+	('Sub-saharan Africa','Southern Africa','South Africa',2020,92.0,78.67,58.004567,337.62,1213090.0,24.34),
+	('Sub-saharan Africa','Southern Africa','Namibia',2020,92.0,78.67,58.004567,337.62,1213090.0,NULL),
+	('Sub-saharan Africa','Central Africa','Burundi',2020,92.0,78.67,58.004567,337.62,1213090.0,24.34)
+;
